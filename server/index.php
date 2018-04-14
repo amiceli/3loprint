0e662cc845c4f4c8764c2742f9eaaee123578565
MICELI Antoine <amiceli@mailinblack.com>
2018-04-14 15:57:26 +0200

    Add server source files.

---
+++ server/index.php
@@ -0,0 +1,46 @@
+<?php
+
+use Dompdf\Dompdf;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('./view/');
+$twig = new Twig_Environment($loader, array(
+    'cache' => './cache',
+    'auto_reload' => true
+));
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+$klein = new \Klein\Klein();
+
+$klein->respond('GET', '/', function () use ($twig) {
+    return $twig->render('app.twig', array('DEV' => getenv('DEV')));
+});
+
+$klein->respond('GET', '/pdf/[:link]', function ($request, $response) {
+    $link = $request->param('link');
+
+    $response->file("./$link.pdf");
+});
+
+$klein->respond('POST', '/generate', function ($request, $response, $service) use ($twig) {
+    $postIts = json_decode($request->body())->postIts;
+
+    $html = $twig->render('index.twig', array('postIts' => $postIts->postIts));
+
+    $dompdf = new Dompdf();
+    $dompdf->loadHtml($html);
+    $dompdf->setPaper('A4', 'landscape');
+    $dompdf->render();
+
+    $output = $dompdf->output();
+    $name = time();
+
+    file_put_contents("$name.pdf", $output);
+
+    return "$name";
+});
+
+$klein->dispatch();
