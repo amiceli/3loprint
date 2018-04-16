b1d4ac0044fd97ee506517874da275c67c521df4
amiceli <miceliantoine@protonmail.com>
2018-04-16 22:03:20 +0200

    Fix ENVIRONMENT variable

--- server/index.php
+++ server/index.php
@@ -4,19 +4,19 @@ use Dompdf\Dompdf;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$dotenv = new \Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
 $loader = new Twig_Loader_Filesystem('./view/');
 $twig = new Twig_Environment($loader, array(
     'cache' => './cache',
     'auto_reload' => true
 ));
 
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
-
 $klein = new \Klein\Klein();
 
 $klein->respond('GET', '/', function () use ($twig) {
-    return $twig->render('app.twig', array('DEV' => getenv('DEV')));
+    return $twig->render('app.twig', array('DEV' => getenv('ENVIRONMENT')));
 });
 
 $klein->respond('GET', '/pdf/[:link]', function ($request, $response) {
