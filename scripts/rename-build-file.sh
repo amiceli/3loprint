0cc49d981c0cdbc3ee410b9c593755cc8c5b0605
amiceli <miceliantoine@protonmail.com>
2018-05-05 20:13:14 +0200

    Update build script and bash script

--- scripts/rename-build-file.sh
+++ scripts/rename-build-file.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # copy css
-cp ${TRAVIS_BUILD_DIR}/server/public/css/app*.css ${TRAVIS_BUILD_DIR}/server/public/css/app.css
+cp ${TRAVIS_BUILD_DIR}/app/dist/css/app*.css ${TRAVIS_BUILD_DIR}/server/public/css/app.css
 
 # copy app js files
-cp ${TRAVIS_BUILD_DIR}/server/public/js/app.*.js ${TRAVIS_BUILD_DIR}/server/public/js/app.js
-cp ${TRAVIS_BUILD_DIR}/server/public/js/app.*.js.map ${TRAVIS_BUILD_DIR}/server/public/js/app.js.map
+cp ${TRAVIS_BUILD_DIR}/app/dist/js/app.*.js ${TRAVIS_BUILD_DIR}/server/public/js/app.js
+cp ${TRAVIS_BUILD_DIR}/app/dist/js/app.*.js.map ${TRAVIS_BUILD_DIR}/server/public/js/app.js.map
 
 # copy manifest map file
-cp ${TRAVIS_BUILD_DIR}/server/public/js/manifest.*.js.map ${TRAVIS_BUILD_DIR}/server/public/js/manifest.js.map
+cp ${TRAVIS_BUILD_DIR}/app/dist/js/manifest.*.js.map ${TRAVIS_BUILD_DIR}/server/public/js/manifest.js.map
 
 # copy vendor js files
-cp ${TRAVIS_BUILD_DIR}/server/public/js/vendor.*.js ${TRAVIS_BUILD_DIR}/server/public/js/vendor.js
-cp ${TRAVIS_BUILD_DIR}/server/public/js/vendor.*.js.map ${TRAVIS_BUILD_DIR}/server/public/js/vendor.js.map
+cp ${TRAVIS_BUILD_DIR}/app/dist/js/vendor.*.js ${TRAVIS_BUILD_DIR}/server/public/js/vendor.js
+cp ${TRAVIS_BUILD_DIR}/app/dist/js/vendor.*.js.map ${TRAVIS_BUILD_DIR}/server/public/js/vendor.js.map
